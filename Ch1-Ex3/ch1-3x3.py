c98267f8594d0a2b15d3267928813083682b92ea
ochen658 <980212chp>
2016-09-06 16:34:57 -0400

    Test commit

---
+++ Ch1-Ex3/ch1-3x3.py
@@ -0,0 +1,13 @@
+#
+# Template for code submission
+# name : Hanping Chen
+# email : hac105@pitt.edu
+# date : 09/06/2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Starting with Python, Chapter 1, Exercise 3
+# Notes:
+# template
