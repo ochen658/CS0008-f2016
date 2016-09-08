5906ae1e0d1dab2327776f1da0168ed1014e979b
ochen658 <980212chp>
2016-09-07 23:42:07 -0400

    Test commit

--- Ch1-Ex3/ch1-3x3.py
+++ Ch1-Ex3/ch1-3x3.py
@@ -10,4 +10,14 @@
 # description of this file goes here
 # Starting with Python, Chapter 1, Exercise 3
 # Notes:
-# template
+#template
+
+meter = input("How many square meters do you have? ")
+print(meter)
+acres = meter/4046.8564224
+print(acres)
+#
+print('The distance the car will travel in 6 hours is',6 * 90)
+print('The distance the car will travel in 10 hours',10 * 90)
+print('The distance the car will travel in 15 hours',15 * 90)
+print('The distance the car will travel in 2 hours and 25 minutes',145/60 * 90)
