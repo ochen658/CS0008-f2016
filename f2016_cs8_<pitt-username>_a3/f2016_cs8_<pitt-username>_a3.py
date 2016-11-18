4cf1b4946b300590136961199f9dd24d71cb6074
ochen658 <980212chp>
2016-11-18 17:40:04 -0500

    assignment 2 python programe

--- f2016_cs8_<pitt-username>_a3/f2016_cs8_<pitt-username>_a3.py
+++ f2016_cs8_<pitt-username>_a3/f2016_cs8_<pitt-username>_a3.py
@@ -1,36 +1,41 @@
 def sort_by_distance(data):
     return data['distance']
 def main():
-    decision = 'Y'
     files_numb=0
     filesbeing_read = []
+    more_than_once = []
     total_distance = 0
     number_of_line = 0
-    while decision == 'Y':
-        files_numb+=1
-        x = input('Which file would you like to open?')
-        y = open(x,'r')
-        for line in y:
+    a = open('monster input file.cvs','w')
+    a.write('/Users/macbookpro/PycharmProjects/CS0008-f2016/Ch1-Ex3/f2016_cs8_a3.data.1.csv\n')
+    a.write('/Users/macbookpro/PycharmProjects/CS0008-f2016/Ch1-Ex3/f2016_cs8_a3.data.2.csv\n')
+    a.write('/Users/macbookpro/PycharmProjects/CS0008-f2016/Ch1-Ex3/f2016_cs8_a3.data.3.csv\n')
+    a.close()
+    y = open('monster input file.cvs','r')
+    for x in y:
+        x = x[0:-1]
+        t = open(x,'r')
+        for line in t:
             number_of_line += 1
             line = line.rstrip()
             line = line.split(',')
-
             try:
                 line[1] = float(line[1])
                 total_distance += line[1]
             except:
                 y = 0
-    #In this part I am trying to determine whether the participants have multiple records, but the output gives
-                #me zero all the time.
             k = 0
-            for t in filesbeing_read:
-                if line[0] in t.values():
+            for s in filesbeing_read:
+                if line[0] in s.values():
                     k = 1
                     try:
-                        d1 = line.get('distance','Error')
+                        d1 = s.get('distance','Error')
                         value = d1 + line[1]
-                        t['distance'] = value
-                        t['multiple'] = 1
+                        s['distance'] = float(value)
+                        if line[0] in more_than_once:
+                            y=0
+                        else:
+                            more_than_once.append(line[0])
                     except:
                         y = 0
             if k == 0:
@@ -38,15 +43,10 @@ def main():
                 new_data['name'] = line[0]
                 new_data['distance'] = line[1]
                 filesbeing_read.append(new_data)
-            x=0
-            for line in filesbeing_read:
-                if len(line)== 3:
-                    x += 1
-        decision = input('Would you you like to open another file?Press "Y" to read file. ')
     filesbeing_read.sort(key = sort_by_distance)
-    print('participants with multiple records',x)
-    print('Number of Input files read',files_numb)
-    print('Total number of lines read',number_of_line-files_numb)
+    print('participants with multiple records',len(more_than_once))
+    print('Number of Input files read',3)
+    print('Total number of lines read',number_of_line-3)
     print('total distance run ',total_distance)
     date = filesbeing_read[-2]
     print('max distance run', date['distance'])
